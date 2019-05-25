63edd81ef88b5db202c2e40ab4b1e5e55ec3dd6a
Paulina Velazquez <mpaulina.velazquez@gmail.com>
2019-05-25 16:47:34 -0400

    1. Set 'searching' to false on JQuery datable 2. Set 'paging' to false on JQuery datatable

--- scripts/initializers/CallTableInitializer.js
+++ scripts/initializers/CallTableInitializer.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
     $('#call-table').DataTable({
         "scrollY": "270px",
         "scrollCollapse": true,
+        "searching": false,
+        "paging": false,
     });
     $('.dataTables_length').addClass('bs-select');
 });
